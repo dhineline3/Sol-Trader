fbe888cfba3e93cf01074066138247461c3b1f33
Daniel Hineline <danielhineline3@outlook.com>
2019-03-13 09:08:36 -0400

    Title Screen/Character Name

--- SpaceTradingGame/Program.cs
+++ SpaceTradingGame/Program.cs
@@ -8,8 +8,21 @@ namespace SpaceTradingGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string CharacterName;
+
+            Console.WriteLine("SOL TRADER");
+            Console.WriteLine("Welcome to Sol Trader young adventurer!");
+            Console.WriteLine("The year is 2147. Exploring the stars has become a hobby for many as space travel becomes commonplace.");
+            Console.WriteLine("You have just graduated from the stellar business academy with a degree in intergalatic finance! Congratulations!");
+            Console.WriteLine("Your father rewards you with your first spaceship. Will you fulfill your dreams of becoming a space trading mogul?");
+            Console.WriteLine("Or will you die alone, penniless, drifting through space, living through your dreams in crysosleep?");
+            Console.WriteLine("Choose your path. Your journey begins now!");
+            Console.Write("Please enter your character name: ");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Great! Your character name is now " + CharacterName + ". Good Luck!");
+            Console.ReadKey();
         }
     }
 }
