31bb124ae807dadb1d8698e46054115c58e420c9
Daniel Hineline <danielhineline3@outlook.com>
2019-03-13 12:08:50 -0400

    Title Page

--- SpaceTradingGame/Program.cs
+++ SpaceTradingGame/Program.cs
@@ -8,35 +8,67 @@ namespace SpaceTradingGame
 {
     public static class Game
     {
+
+
+
+
+
+
         //character name
         static string CharacterName = "Daniel Hineline";
 
         //print out game title and overview
         public static void StartGame()
         {
+
+
+
+
+
             //the next few lines will have your own content
-            Console.WriteLine("SOL TRADER");
-            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+           Console.WriteLine(@"
+
+                                         _______.  ______    __         .___________..______          ___       _______   _______ .______      
+                                        /       | /  __  \  |  |        |           ||   _  \        /   \     |       \ |   ____||   _  \     
+                                       |   (----`|  |  |  | |  |        `---|  |----`|  |_)  |      /  ^  \    |  .--.  ||  |__   |  |_)  |    
+                                        \   \    |  |  |  | |  |            |  |     |      /      /  /_\  \   |  |  |  ||   __|  |      /     
+                                    .----)   |   |  `--'  | |  `----.       |  |     |  |\  \----./  _____  \  |  '--'  ||  |____ |  |\  \----.
+                                    |_______/     \______/  |_______|       |__|     | _| `._____/__/     \__\ |_______/ |_______|| _| `._____|
+
+                                                                                                           ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press enter to continue...");
+
+            Console.ReadKey();
+            Console.Clear();
+
             Console.WriteLine("Welcome to Sol Trader young adventurer!");
             Console.WriteLine("The year is 2147. Exploring the stars has become a hobby for many as space travel becomes commonplace.");
             Console.WriteLine("You have just graduated from the stellar business academy with a degree in intergalatic finance! Congratulations!");
             Console.WriteLine("Your father rewards you with your first spaceship. Will you fulfill your dreams of becoming a space trading mogul?");
-            Console.WriteLine("Or will you die alone, penniless, drifting through space, living through your dreams in crysosleep?");
-            Console.WriteLine();
-            Console.WriteLine("Choose your path. Your journey begins now!");
-            Console.WriteLine();
+            Console.WriteLine("Or will you die alone, penniless, drifting through space, living through your dreams in crysosleep?\n");
+            Console.WriteLine("Choose your path. Your journey begins now!\n");
+            Console.WriteLine("Press 'Enter' to continue.");
+            Console.ReadLine();
+            Console.Clear();
             NameCharacter();
+
         }
         //ask player for a name and save it
-        static void NameCharacter()
+        public static void NameCharacter()
         {
 
             Console.Write("Please enter your character name: ");
             CharacterName = Console.ReadLine();
-            Console.WriteLine();
-            Console.WriteLine("Great! Your character name is now " + CharacterName + ". Good Luck!");
+            Console.WriteLine($"\nGreat! Your character name is now {CharacterName}. Good Luck!");
             Console.ReadKey();
+
         }
+        //original dialog method
+
+
 
     }
     class Item
@@ -53,3 +85,4 @@ namespace SpaceTradingGame
         }
     }
 }
+
