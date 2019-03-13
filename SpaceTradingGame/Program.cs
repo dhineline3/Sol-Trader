1b28148c3f7a70247a8a3723a101159db2ffd6ff
Daniel Hineline <danielhineline3@outlook.com>
2019-03-13 09:36:34 -0400

    Added Classes

--- SpaceTradingGame/Program.cs
+++ SpaceTradingGame/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace SpaceTradingGame
 {
+    class Game
+    {
+
+    }
+    class Item
+    {
+
+    }
     class Program
     {
         static void Main()
@@ -13,17 +21,20 @@ namespace SpaceTradingGame
             string CharacterName;
 
             Console.WriteLine("SOL TRADER");
+            Console.WriteLine();
             Console.WriteLine("Welcome to Sol Trader young adventurer!");
             Console.WriteLine("The year is 2147. Exploring the stars has become a hobby for many as space travel becomes commonplace.");
             Console.WriteLine("You have just graduated from the stellar business academy with a degree in intergalatic finance! Congratulations!");
             Console.WriteLine("Your father rewards you with your first spaceship. Will you fulfill your dreams of becoming a space trading mogul?");
             Console.WriteLine("Or will you die alone, penniless, drifting through space, living through your dreams in crysosleep?");
+            Console.WriteLine();
             Console.WriteLine("Choose your path. Your journey begins now!");
+            Console.WriteLine();
             Console.Write("Please enter your character name: ");
             CharacterName = Console.ReadLine();
+            Console.WriteLine();
             Console.WriteLine("Great! Your character name is now " + CharacterName + ". Good Luck!");
             Console.ReadKey();
-            Console.WriteLine("Daniel Hineline is Awesome!");
         }
     }
 }
